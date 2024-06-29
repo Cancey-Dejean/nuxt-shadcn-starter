c56055b66d060e5779859db7a99b59dd2a4578a0
Cancey Dejean <canceyd@gmail.com>
2024-06-29 10:34:06 -0500

    added clean starter files

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: "default",
+});
+
+useHead({
+  title: "Homepage",
+});
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold underline">Homepage</h1>
+</template>
